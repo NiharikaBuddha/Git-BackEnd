f6f0a3e53182aa222c5b340d933a1bdd6e213170
NiharikaBuddha <niharikabuddha998@gmail.com>
2023-07-19 11:28:13 +0530

    Lets Revise Javascript concepts(Module 1-2)

---
+++ Module1/LetsReviseJavascriptconcepts.js
@@ -0,0 +1,13 @@
+// arrow function which returns the product of two numbers
+const multiply = (a, b) => a * b;
+console.log(multiply(1,2));
+
+// Create a student object
+const student = {
+    firstName: 'Niha',
+    lastName: 'Buddha',
+    age: 24,
+    grade: 'A',
+  };
+  // Print the student object
+  console.log(student);
