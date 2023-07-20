980f84f8b757c414a175fd236598b23f37b4b71d
NiharikaBuddha <niharikabuddha998@gmail.com>
2023-07-20 20:28:23 +0530

    Writing and reading from a file (Module 1-8 Part2)

--- Writing and reading from a file.js
+++ Writing and reading from a file.js
@@ -9,14 +9,38 @@ const server = http.createServer((req,res)=>{
         res.write('<header><title>Enter Message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type ="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
+
+        // Read messages from the file and display them at the top of the form
+        const messages = fs.readFileSync('message.txt', 'utf8').split('\n');
+        messages.forEach(message => {
+            if (message !== '') {
+                res.write(`<p>${message}</p>`);
+            }
+        });
+
+        res.write('</body>');
+        res.write('</html>');
+
         return res.end();
     }
 
-    if(url === '/message' && method == 'POST')
+    if(url === '/message' && method === 'POST')
     {
-        fs.writeFileSync('message.txt','DUMMY');
-        res.statusCode=302;
-        res.setHeader('location','/');
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            
+            // Append the new message to the file
+            fs.appendFileSync('message.txt', message + '\n');
+
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        });
         return res.end();
     }
 
