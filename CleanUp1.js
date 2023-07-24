f45b2dc28de3b22e98d0face8eaa85a524bd859f
NiharikaBuddha <niharikabuddha998@gmail.com>
2023-07-24 13:23:41 +0530

    Routers and Filters

--- CleanUp1.js
+++ CleanUp1.js
@@ -1,43 +1,21 @@
 const http = require('http');
 
 const express = require('express');
-
+const Some_var = express();
 const bodyParser = require('body-parser');
 
-const Some_var = express();
-
-// Some_var.use((req,res,next)=>{
-//     console.log('In the middleware');
-//     next();     //Allows the request to continue to the next middleware in line
-// });
+const adminRouters = require('./Routers/Admin');
+const shopRouters = require('./Routers/Shop');
 
 Some_var.use(bodyParser.urlencoded({extended: false}));
 
-Some_var.use('/',(req,res,next)=>{
-    console.log('This always runs');
-    next();
+Some_var.use('/Admin', adminRouters);
+Some_var.use(shopRouters);
+
+Some_var.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
-Some_var.use('/AddProduct',(req,res,next)=>{
-    console.log('In another middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></input></form>');
-});
 
-Some_var.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-
-Some_var.use('/',(req,res,next)=>{
-    console.log('In another middleware');
-    res.send('<h1>Hello from express</h1>');
-});
-
-const server = http.createServer(Some_var);
-
-// const CleanUp = require('./CleanUp');
-
-// const server = http.createServer(CleanUp);
-
-// server.listen(3000);  
+const server = http.createServer(Some_var);  
 server.listen(4000);
---
+++ Routers/Admin.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/Admin/AddProduct',(req,res,next)=>{
+    res.send('<form action="/Admin/Addproduct" method="POST"><input type="text" name="title"><button type="submit">Add product</button></input></form>');
+});
+
+router.post('/Addproduct',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports=router;
